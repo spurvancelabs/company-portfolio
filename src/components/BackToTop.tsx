dedd41b58ef7bde0a232711c8e3a1844de30c697
Abdullah Khaver <muhammadabdullahkhaver@gmail.com>
2026-01-07 14:00:21 -0500

    components update

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -25,6 +25,7 @@ const BackToTop = () => {
       onClick={scrollToTop}
       aria-label="Back to top"
       className={`
+        cursor-pointer
         fixed bottom-6 right-6 z-50
         flex items-center justify-center
         w-12 h-12 rounded-full
@@ -36,7 +37,7 @@ const BackToTop = () => {
         ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-6 pointer-events-none"}
       `}
     >
-      <ArrowUp className="w-5 h-5" />
+      <ArrowUp className="cursor-pointer w-5 h-5" />
     </button>
   );
 };
